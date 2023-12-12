036d03c55eb9c510f7833e44a3feb079898dd6fe
Ajayi Sekemi <ajesy2k21@gmail.com>
2023-12-12 07:24:35 -0500

    nw

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./app.scss";
-import Contact from "./components/Contact/Contact";
+import Contact from "./components/contact/contact";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Parallax from "./components/Parallax/Parallax";
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-animated-portfolio/"
+  base: "/react-animated-portfolio"
 })
